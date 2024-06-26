b87121438b2357be6abae342269e1eb643aa9132
SanvuSarvesh <sarveshkr1308@gmail.com>
2024-06-26 21:30:21 +0530

    added permission controller

---
+++ src/main/java/com/project/realtime_collaborative_doc_editing/service/impl/PermissionServiceImpl.java
@@ -0,0 +1,36 @@
+package com.project.realtime_collaborative_doc_editing.service.impl;
+
+import com.project.realtime_collaborative_doc_editing.common.BaseResponse;
+import com.project.realtime_collaborative_doc_editing.service.PermissionService;
+
+public class PermissionServiceImpl implements PermissionService {
+    @Override
+    public BaseResponse allowUserToView() {
+        return null;
+    }
+
+    @Override
+    public BaseResponse allowUserToEdit() {
+        return null;
+    }
+
+    @Override
+    public BaseResponse withDrawViewPermission() {
+        return null;
+    }
+
+    @Override
+    public BaseResponse withEditViewPermission() {
+        return null;
+    }
+
+    @Override
+    public BaseResponse requestForViewAccess() {
+        return null;
+    }
+
+    @Override
+    public BaseResponse requestForEditAccess() {
+        return null;
+    }
+}
